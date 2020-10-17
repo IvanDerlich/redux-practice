5ef5152da1e39466f65ac28cd15453a7b835c87c
Ivan Derlich <e@ivanderlich.com>
2020-10-16 23:16:37 -0300

    Lesson 10

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux'
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
 
 const counter = (state = 0, action) => {
   switch (action.type){
@@ -43,3 +45,52 @@ const render = () => {
 }
 store.subscribe(render)
 render()
+
+
+const addCounter = (list) => [...list,0]
+
+const removeCounter = (list, index) => [
+    ...list.slice(0,index)
+    ,...list.slice(index+1)
+  ];
+
+const incrementCounter = (list, index) => [
+  ...list.slice(0,index)
+  ,list[index]+1
+  ,...list.slice(index+1)
+];
+
+const testAddcounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+  deepFreeze(listBefore);
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+}
+
+const testRemoveCounter = () => {
+  const listBefore = [0,10, 20]
+  const listAfter = [0, 20]
+  deepFreeze(listBefore)
+
+  expect(
+    removeCounter(listBefore,1)
+  ).toEqual(listAfter);
+}
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+  deepFreeze(listBefore);
+
+  expect(
+    incrementCounter(listBefore,1)
+  ).toEqual(listAfter);
+}
+
+testAddcounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed.');
