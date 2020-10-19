c55b48d44abc4c8a9f4fa7c42e77db63769063ce
Ivan Derlich <e@ivanderlich.com>
2020-10-19 18:38:59 -0300

    Lesson 12 finished

--- src/index.js
+++ src/index.js
@@ -24,13 +24,23 @@ const todos = (state = [], action) => {
           text: action.text,
           completed: false,
         }
-      ]    
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if( todo.id !== action.id){
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: true
+        }
+      })
     default:
       return state;
   }
 }
 
-const testAddtodo = () => {
+const testAddTodo = () => {
   const stateBefore = [];
   const action = {
     type: 'ADD_TODO',
@@ -52,5 +62,42 @@ const testAddtodo = () => {
     
 }
 
-testAddtodo()
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id:0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id:1,
+      text: 'Go shopping',
+      completed: false,
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id:0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id:1,
+      text: 'Go shopping',
+      completed: true,
+    }
+  ];
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+  expect(
+    todos(stateBefore,action)
+  ).toEqual(stateAfter)
+}
+
+testAddTodo()
+testToggleTodo()
 console.log('All tests passed.');
