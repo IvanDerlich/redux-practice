1ad7dfa9ab8d3dca77f869695d43eb5d785f9cbc
Ivan Derlich <e@ivanderlich.com>
2020-10-20 17:31:24 -0300

    Lesson 13 finished

--- src/index.js
+++ src/index.js
@@ -106,6 +106,47 @@ const testToggleTodo = () => {
     todos(stateBefore,action)
   ).toEqual(stateAfter)
 }
+const store = createStore(todos)
+
+console.log('Initial state:')
+console.log(store.getState())
+console.log('-------------')
+
+console.log('Dispatching ADD_TODO');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+
+console.log('Current state:')
+console.log(store.getState())
+console.log('-------------')
+
+console.log('Dispatching ADD_TODO');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Go Shopping'
+});
+
+console.log('Current state:')
+console.log(store.getState())
+console.log('-------------')
+
+console.log('Current state:')
+console.log(store.getState())
+console.log('-------------')
+
+console.log('Dispatching TOGGLE_TODO');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0,
+});
+
+console.log('Current state:')
+console.log(store.getState())
+console.log('-------------')
 
 testAddTodo()
 testToggleTodo()
