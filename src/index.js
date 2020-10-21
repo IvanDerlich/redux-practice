5e7254876c2bdcdf0e3809df9a0b50b5ecc6c9c2
Ivan Derlich <e@ivanderlich.com>
2020-10-20 21:16:54 -0300

    Lesson 14 finished

--- src/index.js
+++ src/index.js
@@ -40,12 +40,31 @@ const todos = (state = [], action) => {
     case 'ADD_TODO':
       return [
         ...state,
-        todo(undefined,action)
+        todo(undefined,action) 
       ]
-    case 'TOGGLE_TODO':
+    case 'TOGGLE_TODO': 
       return state.map(t => todo(t, action))
     default:
       return state;
+  }
+}
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch(action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action)
   }
 }
 
@@ -106,7 +125,7 @@ const testToggleTodo = () => {
     todos(stateBefore,action)
   ).toEqual(stateAfter)
 }
-const store = createStore(todos)
+const store = createStore(todoApp)
 
 console.log('Initial state:')
 console.log(store.getState())
@@ -147,6 +166,17 @@ store.dispatch({
 console.log('Current state:')
 console.log(store.getState())
 console.log('-------------')
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED',
+});
+
+console.log('Current state:')
+console.log(store.getState())
+console.log('-------------')
+
 
 testAddTodo()
 testToggleTodo()
